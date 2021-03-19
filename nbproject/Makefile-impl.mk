946f5625a00940247624b945bb5c1c70d8314704
Ratik Kapoor <ratikrk@gmail.com>
2021-03-19 15:13:42 -0600

    feat: Added CAN receive functionality

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=canbus-mcu-distance-sensor
+PROJECTNAME=canbus-mcu-distance-sensor.X
 
 # Active Configuration
 DEFAULTCONF=default
