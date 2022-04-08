7fb2f8c2269103f18eeb215faf52bf8bf4eeabf4
Sahil Bhatt <78242221+sahilb1017@users.noreply.github.com>
2022-04-08 14:11:34 -0600

    Delete sensorworking/mcu-canbus-distance-sensor directory

--- sensorworking/mcu-canbus-distance-sensor/build/default/production/distance-sensor.p1.d
+++
@@ -1,6 +0,0 @@
-build/default/production/distance-sensor.p1:  \
-distance-sensor.c  \
-mcc_generated_files/pin_manager.h  \
-mcc_generated_files/tmr0.h  \
-mcc_generated_files/tmr2.h  \
-mcc_generated_files/device_config.h 
